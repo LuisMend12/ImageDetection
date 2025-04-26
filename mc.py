27da51ce6fa0f3b70ab1d8cc00ed1abb53372829
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-04-26 00:08:20 -0400

    new

---
+++ mc.py
@@ -0,0 +1,27 @@
+import random
+
+class FairRouletter():
+    def __init__(self):
+        self.pockets = []
+        for i in range(1, 37):
+            self.pockets.append(i)
+        self.ball = None
+        self.pocketOdds  = len(self.pockets) - 1
+    
+    def spin(self):
+        self.ball = random.choice(self.pockets)
+
+    def betPocket(self, pocket, amt):
+        if str(pocket) == str(self.ball):
+            return amt*self.pocketOdds
+        else: return -amt
+
+    def __str__(self):
+        return 'Fair Roulette'
+
+
+    def playRouletter(game, numSpins, pocket, bet, toPrint):
+        totPocket = 0
+        for i in range(numSpins):
+            game.spin()
+            totPocket += game.betPocket(pocket, bet)
