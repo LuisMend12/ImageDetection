28e951960c0dcd37d478d6e78f1c962dcc6dfc8e
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-04-26 00:17:31 -0400

    monte_carlo_simulation

--- mc.py
+++ mc.py
@@ -7,21 +7,33 @@ class FairRouletter():
             self.pockets.append(i)
         self.ball = None
         self.pocketOdds  = len(self.pockets) - 1
-    
+
     def spin(self):
         self.ball = random.choice(self.pockets)
 
     def betPocket(self, pocket, amt):
         if str(pocket) == str(self.ball):
-            return amt*self.pocketOdds
-        else: return -amt
+            return amt * self.pocketOdds
+        else:
+            return -amt
 
     def __str__(self):
         return 'Fair Roulette'
 
 
-    def playRouletter(game, numSpins, pocket, bet, toPrint):
-        totPocket = 0
-        for i in range(numSpins):
-            game.spin()
-            totPocket += game.betPocket(pocket, bet)
+def playRouletter(game, numSpins, pocket, bet, toPrint):
+    totPocket = 0
+    for i in range(numSpins):
+        game.spin()
+        totPocket += game.betPocket(pocket, bet)
+    if toPrint:
+        print(numSpins, 'spins of', game)
+        print('Expected return betting', pocket, '=',
+              str(100 * totPocket / numSpins) + '%\n')
+    return totPocket / numSpins
+
+
+
+game = FairRouletter()
+for numSpins in (100, 100000):
+    playRouletter(game, numSpins, 2, 1, True)
